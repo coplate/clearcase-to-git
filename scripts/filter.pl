457fd5b15b36f597d21c8be0e71cee3661303e78
Matthew Carlson <matthew.carlson@gmail.com>
2019-05-08 13:00:25 -0500

    Added scripts to use in windows batch

--- scripts/filter.pl
+++ scripts/filter.pl
@@ -1,33 +1,62 @@
-#!/usr/bin/perl -n
-
-BEGIN {
-    $file_ending = qr/\@\@(\\main(\\[\w\.]+)*\\\d+)?\r$/;
-    @patterns = (
-        # we need to know all interesting elements, even if they exist only in checkedout directories, but we skip checkedout *versions* (incremental export)
-	qr/CHECKEDOUT\r$/,
-	# general directories
-	qr/\\directory_not_wanted/,
-	qr/lost\+found/,
-
-	# files
-	qr/\.ccexclude$file_ending/,
-	qr/\.mkelem$file_ending/,
-	qr/\.\w+\.user$file_ending/,
-	qr/\.suo$file_ending/,
-	qr/\.contrib$file_ending/,
-	qr/\.keep$file_ending/);
+use strict;
+my $type = shift;
+my $vob = shift;
+my $project = shift;
+my $list_of_files = shift;
+my $file_ending = qr/\@\@(\\main(\\[\w\.]+)*\\\d+)?\r$/;
+my @dir_patterns = (
+  # we need to know all interesting elements, even if they exist only in checkedout directories, but we skip checkedout *versions* (incremental export)
+  qr/CHECKEDOUT\r$/,
+  # general directories
+  qr/ORIGINAL_SOURCE/,
+  qr/(DATABASE|database)/,
+  qr/transfer/,
+  qr/mockups/,
+  qr/cgi-logs/
+);
+my @file_patterns = (
+  # files
+  qr/README\.md/,
+  qr/\.bak$/, # infidividual file patters
+  qr/[\\]ORIGINAL_SOURCE[\\]/,  # files in forbitten directories
+  qr/[\\](DATABASE|database)[\\]/,
+  qr/[\\]transfer[\\]/,
+  qr/[\\]mockups[\\]/,
+  qr/[\\]cgi-logs[\\]/
+);
+my $patterns = \@dir_patterns;
+if( $type eq 'F' ){
+  $patterns = \@file_patterns;
 }
-
-$skip = 0;
-
-foreach $pattern (@patterns) {
-    if (/$pattern/) {
-	$skip = 1;
-	last;
+my $seen_dot = 0;
+print STDERR "Removing ^$vob\\$project\r\n";
+open(my $fh, '<', $list_of_files) || die "cannot open $_";
+while(my $path = <$fh>){
+  $path =~ s/^\Q$vob\E[\\]//i; # turn  #K:\view\vob\project@@ into project@@, K:\view\vob\.@@ to .@@
+        
+        
+  my $skip = 0;
+  #print $path."\r\n";
+  if( not $path =~ '^[.][@][@]' ){
+    if( not $path =~ /^\Q$project\E/i ){
+      next; # skip lost_+found, airbp, etc
+    }else{
+      if( $seen_dot == 0 ){
+        print '.@@'."\r\n"; # for certain reasons, the script does not work reliably if the parent folder K:\view\vob\.@@ is n ot present, so this adds it.
+        $seen_dot = 1;
+      }
     }
-}
-
-next if $skip;
-
-s/.*\\MyVob\\//;
-print
+  }else{
+    $seen_dot = 1;
+  }
+       
+       
+  foreach my $pattern (@{$patterns}) {
+    if ($path =~ /$pattern/) {
+      $skip = 1;
+      last;
+    }
+  }
+        
+  next if $skip;
+  print $path;
