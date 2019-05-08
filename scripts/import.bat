57314d214707c6fad6cea90d602945b65769cd02
Matthew Carlson <coplate@gmail.com>
2019-05-08 13:21:53 -0500

    Update import.bat

--- scripts/import.bat
+++ scripts/import.bat
@@ -2,11 +2,14 @@
 set batch_location=%~dp0
 set source_location=%batch_location%\..\
 
-copy %source_location%\App.config %source_location%\scripts\GitImporter.exe.config
-copy %source_location%\protobuf-net.* %source_location%\scripts\
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe  /debug /define:DEBUG /define:TRACE /r:protobuf-net.dll /out:%batch_location%\GitImporter.exe /pdb:%batch_location%\GitImporter %source_location%\*.cs 
-
-goto err
+if not exist %batch_location%\GitImporter.exe (
+ copy %source_location%\App.config %source_location%\scripts\GitImporter.exe.config
+ copy %source_location%\protobuf-net.* %source_location%\scripts\
+ C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe  /debug /define:DEBUG /define:TRACE /r:protobuf-net.dll /out:%batch_location%\GitImporter.exe /pdb:%batch_location%\GitImporter %source_location%\*.cs 
+)
+if not exist %batch_location%\GitImporter.exe (
+ goto err
+)
 
 if [%1]==[] goto usage
 if [%2]==[] goto usage
